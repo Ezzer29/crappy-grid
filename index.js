a9a2ebe85b6a1829863b9492e6ccd9b04ba08675
Ezzer29 <ezzerelectron@gmail.com>
2022-01-05 01:02:33 +0200

    made a bit a cleaning

--- index.js
+++ index.js
@@ -13,27 +13,50 @@ function cursor (x) { return x + 2500; }
 // function cursorY (y) { return y + 2500; }
 
 // prepare
-for ( let i = -2500; i < 2500; i += 1 ) {
-    for ( let j = -2500; j < 2500; j += 1 ) {
-        if ( i == 0 || j == 0 ) a[cursor(i)][cursor(j)] = '5';
+function prep() {
+    for ( let i = -2500; i < 2500; i += 1 ) {
+        for ( let j = -2500; j < 2500; j += 1 ) {
+            if ( i == 0 || j == 0 ) a[cursor(i)][cursor(j)] = 5;
+            else a[cursor(i)][cursor(j)] = 0;
+        }
     }
 }
 
 function write (x, y, k) {
-    a[Math.round(cursor(y))][Math.round(cursor(x))] = k;
+    try { a[Math.round(cursor(y))][Math.round(cursor(x))] = k; }
+    catch (err) { console.error('Da ErRor IN func write.. (y, x, cy, cx): ', y, x, cursor(y), cursor(x)); console.error(err); }
+}
+
+function imgMagic (name) {
+    Jimp.read('input.png', (err, img) => {
+        if (err) throw err;
+        img.resize(5000, 5000);
+        for ( let i = 0; i < 5000; i += 1 ) {
+            for ( let j = 0; j < 5000; j += 1 ) {
+                if ( a[i][j] == 5 || a[i][j] == -5 ) {
+                    img.setPixelColor(color_1, j, i);
+                } else if ( a[i][j] == 10 ) {
+                    img.setPixelColor(color_2, j, i);
+                } else {
+                    img.setPixelColor(color_0, j, i);
+                }
+            }
+        }
+        img.write(name + '.png');
+    });
 }
 
 // Modules
 
-const zoom = 1500;
+const zoom = 1500; // more like scale, but anyways...
 const step = .00001;
 
-function crown () {
+function crown (x) {
     for ( let t = 0; t <= 2*Math.PI; t += step ) {
         // write(Math.cos(t) * zoom, Math.sin(t) * zoom, 10);
         let sum1 = 0;
         let sum2 = 0;
-        let r1 = -10;
+        let r1 = x;
         let r2 = 5;
         let c = 3;
         for ( let i = 0; i <= c; i += 1 ) {
@@ -46,23 +69,6 @@ function crown () {
     }
 }
 
-crown();
-
-// My SHIT ends here...
-
-Jimp.read('input.png', (err, img) => {
-    if (err) throw err;
-    img.resize(5000, 5000);
-    for ( let i = 0; i < 5000; i += 1 ) {
-        for ( let j = 0; j < 5000; j += 1 ) {
-            if ( a[i][j] == 5 || a[i][j] == -5 ) {
-                img.setPixelColor(color_1, j, i);
-            } else if ( a[i][j] == 10 ) {
-                img.setPixelColor(color_2, j, i);
-            } else {
-                img.setPixelColor(color_0, j, i);
-            }
-        }
-    }
-    img.write('output.png');
-});
+prep();
+crown(10);
+imgMagic('output');
